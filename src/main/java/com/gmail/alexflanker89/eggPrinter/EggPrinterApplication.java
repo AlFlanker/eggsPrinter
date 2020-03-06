5ebadffe546a7ef78fc98639d093d6f1b9b25e07
AlexFlanker <alexflanker89@gmail.com>
2020-03-06 23:22:45 +0300

    master Init commit

---
+++ src/main/java/com/gmail/alexflanker89/eggPrinter/EggPrinterApplication.java
@@ -0,0 +1,13 @@
+package com.gmail.alexflanker89.eggPrinter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EggPrinterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EggPrinterApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/gmail/alexflanker89/eggPrinter/EggPrinterApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gmail.alexflanker89.eggPrinter;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EggPrinterApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
